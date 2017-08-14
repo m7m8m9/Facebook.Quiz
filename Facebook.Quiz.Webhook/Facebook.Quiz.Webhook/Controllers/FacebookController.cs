c3056b45f1bee79853ee411a41a87585870aa2a7
m7m8m9 <mak7mak8mak9@gmail.com>
2017-08-14 23:06:07 +0300

    BK: Create web api project with initial FB webhook debugging.

---
+++ Facebook.Quiz.Webhook/Facebook.Quiz.Webhook/Controllers/FacebookController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Facebook.Quiz.Webhook.Controllers
+{
+    public class FacebookController : ApiController
+    {
+        [HttpGet]
+        public string HubChallenge([FromUri(Name = "hub.mode")]string mode, [FromUri(Name = "hub.challenge")]string challenge, [FromUri(Name = "hub.verify_token")]string token)
+        {
+            return challenge;
+        }
+
+    }
+}
