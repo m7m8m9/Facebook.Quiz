451de0027068e1a22acfb47cb980c35ddacad185
m7m8m9 <mak7mak8mak9@gmail.com>
2017-08-14 22:25:56 +0300

    BK: Create web api project

--- Facebook.Quiz.Webhook/Facebook.Quiz.Webhook/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Facebook.Quiz.Webhook.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Facebook.Quiz.Webhook/Facebook.Quiz.Webhook/Global.asax.cs
+++ Facebook.Quiz.Webhook/Facebook.Quiz.Webhook/Global.asax.cs
@@ -16,8 +16,6 @@ namespace Facebook.Quiz.Webhook
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
